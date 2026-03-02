d16694aeefe379946ac17903c22026779636d11d
Markiyan Pyts <markiyan.pyts@gmail.com>
2026-03-02 15:26:18 +0200

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 
@@ -5,15 +6,16 @@ def main():
     duration = 120  # seconds
     interval = 10
     elapsed = 0
+    test_env_var = os.environ.get("TEST_ENV_VAR", "")
 
-    print(f"Job started. Running for {duration // 60} minutes...")
+    print(f"Job started. Running for {duration // 60} minutes... [{test_env_var}]")
 
     while elapsed < duration:
         time.sleep(interval)
         elapsed += interval
-        print(f"Progress: {elapsed}/{duration}s ({int(elapsed / duration * 100)}%)")
+        print(f"Progress: {elapsed}/{duration}s ({int(elapsed / duration * 100)}%) [{test_env_var}]")
 
-    print("Job completed successfully.")
+    print(f"Job completed successfully. [{test_env_var}]")
 
 
 if __name__ == "__main__":
